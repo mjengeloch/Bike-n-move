046a6a3ea1ba417121db2a6d59503ccf43462e53
MJEngeloch <mariejo.engeloch@gmail.com>
2019-05-08 12:24:03 +0200

    mise en place du JQuery du diapo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,34 +1,72 @@
-/*---HEADER---*/
-$("header").click(function () {
-    $("header").animate({
-        bottom: "100%",
-        opacity: "0.1",
-    }, 2000)
-})
+$(document).ready(function () {
+
+    /*---HEADER---*/
+    $("header").click(function () {
+        $("header").animate({
+            bottom: "100%",
+            opacity: "0.1",
+        }, 2000)
+    })
 
 
-/*---CAROUSEL---*/
-$('.carousel').carousel({
-    interval: 5000
-})
+    /*---CAROUSEL---*/
 
-$("#playButton").hide()
+    var $carrousel = $("#carrousel"),
+        $diapo = $(".diapositive"),
+        indexDiapo = $diapo.length - 1,
+        i = 0,
+        $currentDiapo = $diapo.eq(i);
 
-$('#playButton').click(function () {
+    $diapo.css("display", "none");
+    $currentDiapo.css("display", "block");
+
+    $(".next").click(function () {
+        i++;
+        if (i <= indexDiapo) {
+            $diapo.css("display", "none");
+            $currentDiapo = $diapo.eq(i);
+            $currentDiapo.css("display", "block");
+        } else {
+            i = 0;
+        }
+    });
+
+    $(".prev").click(function () {
+        i--;
+        if (i >= 0) {
+            $diapo.css("display", "none");
+            $currentDiapo = $diapo.eq(i);
+            $currentDiapo.css("display", "block");
+        } else {
+            i = $diapo.length;
+        }
+    });
+
+
+    /* $('.carousel').carousel({
+        interval: 5000
+    })
+    
     $("#playButton").hide()
-    $("#pauseButton").show()
-    $('#carouselFonctionnement').carousel('cycle');
-});
-$('#pauseButton').click(function () {
-    $("#playButton").show()
-    $("#pauseButton").hide()
-    $('#carouselFonctionnement').carousel('pause');
-});
+    
+    $('#playButton').click(function () {
+        $("#playButton").hide()
+        $("#pauseButton").show()
+        $('#carouselFonctionnement').carousel('cycle');
+    });
+    $('#pauseButton').click(function () {
+        $("#playButton").show()
+        $("#pauseButton").hide()
+        $('#carouselFonctionnement').carousel('pause');
+    }); */
 
-// Uncomment to enable Bootstrap tooltips
-// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
-// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+    // Uncomment to enable Bootstrap tooltips
+    // https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
+    // $(function () { $('[data-toggle="tooltip"]').tooltip(); });
 
-// Uncomment to enable Bootstrap popovers
-// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
-// $(function () { $('[data-toggle="popover"]').popover(); });
+    // Uncomment to enable Bootstrap popovers
+    // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
+    // $(function () { $('[data-toggle="popover"]').popover(); });
+
+
+})
